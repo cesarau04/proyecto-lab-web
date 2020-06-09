2aa63537e764169966516455fb9005c90c0731aa
adler zamora <adlerzamora@gmail.com>
2020-06-09 09:56:02 -0500

    Navbar with CSS conditional

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ import './StayNear.css';
 
 // LOGICA DE FIREBASE
 
-const NavBar = () => {
+const NavBar = (props) => {
     return (
         <div className="nav-bar">
             <div className="left">
                 <ul>
-                    <li>Home</li>
-                    <li>People</li>
-                    <li>About</li>
-                    <li>Help</li>
+                    <li className={(props.dark === 'True'?'DarkNav':'')+ " " +(props.current=== 'Host'?'Active':'')}>Become a Host</li>    
+                    <li className={(props.dark === 'True'?'DarkNav':'')+ " " +(props.current=== 'Help'?'Active':'')}>Help</li>
+                    <li className={(props.dark === 'True'?'DarkNav':'')+ " " +(props.current=== 'Sign'?'Active':'')}>Sign up</li>
+                    <li className={(props.dark === 'True'?'DarkNav':'')+ " " +(props.current=== 'Log'?'Active':'')}> Log in</li>
                 </ul>
             </div>
 
