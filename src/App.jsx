982e964e942fdf654bc1b01309f8e719c5fb907b
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-06-08 23:56:08 -0500

    login test
    Signed-off-by: Cesar Augusto Garcia Perez <cesarau04@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <LoginPage />
     </div>
   );
 }
