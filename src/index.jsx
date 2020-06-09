de8bb63798ccc2184c3d5f983e6f41f219f23a86
adler zamora <adlerzamora@gmail.com>
2020-06-09 08:42:30 -0500

    Added Main Background Pattern & MaterializeCSS ref

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import 'materialize-css/dist/css/materialize.min.css';
 import 'typeface-roboto'
 
 ReactDOM.render(
   <React.StrictMode>
     <HomePage />
+    {/* <LoginPage /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
