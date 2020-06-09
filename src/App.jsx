8b5133008fc833ada61fcb8e16d1777b52304fde
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-06-08 22:21:19 -0500

    added buttons to home screen
    Signed-off-by: Cesar Augusto Garcia Perez <cesarau04@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FT from './components/FirebaseTesting'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <FT />
     </div>
   );
 }
