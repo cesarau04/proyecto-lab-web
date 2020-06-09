689b2208d6b12fda5a5e321a03a3fe51c548951d
adler zamora <adlerzamora@gmail.com>
2020-06-09 09:56:56 -0500

    Not important changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'typeface-roboto'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
-    {/* <LoginPage /> */}
+    {/* <HomePage /> */}
+    <SignupPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
