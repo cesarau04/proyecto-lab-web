8d88560f75b48177112f872b41daf4b14eb28fa3
adler zamora <adlerzamora@gmail.com>
2020-06-10 13:20:01 -0500

    keypoint

--- src/components/PlacesSuggestionPage.jsx
+++ src/components/PlacesSuggestionPage.jsx
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import PlaceCard from './PlaceCard';
-import './StayNear.css';
+import React, { useState, useEffect } from "react";
+import PlaceCard from "./PlaceCard";
+import "./StayNear.css";
 
-import myFirebaseInstance from '../firebase/myfirebase'
+import myFirebaseInstance from "../firebase/myfirebase";
 
 const PlacesSuggestionPage = (props) => {
-    const [readDB, setReadDB] = useState(true)
-    const [rooms, setRooms] = useState({})
-    const myFirebase = myFirebaseInstance.getInstance()
+  const [readDB, setReadDB] = useState(true);
+  const [rooms, setRooms] = useState({});
+  const myFirebase = myFirebaseInstance.getInstance();
 
-    useEffect(() => {
-        if (readDB) {
-            myFirebase.database().ref().child("rooms").once('value', 
-            (snapshot) => {
-                setRooms(snapshot.val())
-            });
-            setReadDB(false)
-        }
-    });
+  useEffect(() => {
+    if (readDB) {
+      myFirebase
+        .database()
+        .ref()
+        .child("rooms")
+        .once("value", (snapshot) => {
+          setRooms(snapshot.val());
+        });
+      setReadDB(false);
+    }
+  });
 
-    return (
-        <div className={(props.background === 'true' ? 'card blue-grey darken-1' : '')}>
-            <div className={(props.background === 'true' ? 'card-content white-text' : '')}>
-                {[rooms].map((child, key, arr) => {
-                    console.log(child);
-                    console.log(key);
-                    console.log(arr);
+  return (
+    <div
+      className={props.background === "true" ? "card blue-grey darken-1" : ""}
+    >
+      <div
+        className={props.background === "true" ? "card-content white-text" : ""}
+      >
+        {[rooms].map((child, key, arr) => {
+          console.log(child);
+          console.log(key);
+          console.log(arr);
 
-                    console.log("======================");
-                })}
-                {/* <div className="row">
+          console.log("======================");
+        })}
+        {/* <div className="row">
                     <PlaceCard rowSize={props.rowSize} />
                     <PlaceCard rowSize={props.rowSize} />
                     <PlaceCard rowSize={props.rowSize} />
@@ -38,9 +45,9 @@ const PlacesSuggestionPage = (props) => {
                     <PlaceCard rowSize={props.rowSize} />
                     <PlaceCard rowSize={props.rowSize} />
                 </div> */}
-            </div>
-        </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default PlacesSuggestionPage;
