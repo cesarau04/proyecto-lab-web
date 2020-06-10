7a9a0462bf7165c6a28e9d32551aaf799f27228c
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-06-09 22:48:24 -0500

    added store functionality
    Signed-off-by: Cesar Augusto Garcia Perez <cesarau04@gmail.com>

--- src/components/FirebaseTesting.jsx
+++ src/components/FirebaseTesting.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth"
+import "firebase/storage"
 
+import { connect } from "react-redux";
+import { LogInAction, UserIdAction } from "../actions/index";
 let firebaseConfig = {
     apiKey: "AIzaSyBRlMbi0wdAxTBlL0AjrblnaQ3uJwJK5Ro",
     authDomain: "proyecto-lab-web.firebaseapp.com",
@@ -15,7 +18,7 @@ let firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-const FirebaseTesting = () => {
+const FirebaseTesting = (props) => {
     const db = firebase.database()
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
@@ -61,7 +64,11 @@ const FirebaseTesting = () => {
         if (currentEmail !== "" && currentEmail && currentPassword !== "" && currentPassword) {
             firebase.auth().signInWithEmailAndPassword(currentEmail, currentPassword)
                 .catch((error) => { console.error(error); })
-                .then(((data) => { console.log(data); }))
+                .then((data) => {        
+                    props.dispatch(LogInAction(true))
+                    props.dispatch(UserIdAction(data.user.uid))
+                    console.log("LOG IN")
+                })
         }
     };
 
@@ -69,14 +76,45 @@ const FirebaseTesting = () => {
         let currentEmail = emailRef.current.value
         let currentPassword = passwordRef.current.value
 
-        console.log("EMAIL -> " + currentEmail);
-        console.log("PASSWORD -> " + currentPassword);
         if (currentEmail !== "" && currentEmail && currentPassword !== "" && currentPassword) {
             firebase.auth().createUserWithEmailAndPassword(currentEmail, currentPassword)
                 .catch((error) => { console.error(error); })
-                .then((data) => { console.log(data); })
+                .then((data) => {        
+                    props.dispatch(LogInAction(true))
+                    props.dispatch(UserIdAction(data.user.uid))
+                    console.log("LOG IN - SIGN UP")
+                    const storage = firebase.storage()
+                    storage.ref().child("users").child(data.user.uid).child("profile_pic").putString("64byte profile.png")
+                        // .then((snapshot) => {
+                        //     console.log(snapshot);
+                        //     const db = firebase.database()
+                        //     db.ref().ref('users').child(data.user.uid).child('profile')
+                        // });
+                })
         }
     };
+
+    const handleSignOut = () => {
+        firebase.auth().signOut().then(function () {
+            props.dispatch(LogInAction(false))
+            props.dispatch(UserIdAction(""))
+            console.log("SIGNED OUT ")
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    const handleReduxT = () => {
+        props.dispatch(LogInAction(true))
+    }
+
+    const handleReduxF = () => {
+        props.dispatch(LogInAction(false))
+    }
+
+    useEffect(() => {
+        console.log(props.bIsLoggedIn);
+    });
 
     return (
         <div>
@@ -97,9 +135,18 @@ const FirebaseTesting = () => {
 
                 <button type="submit" onClick={handleSignUp}>Sign Up</button>
                 <button type="submit" onClick={handleLogin}>Login</button>
+                <button type="submit" onClick={handleSignOut}>Sign Out</button>
+                <button type="submit" onClick={handleReduxT}>REDUX True</button>
+                <button type="submit" onClick={handleReduxF}>REDUX False</button>
             </div>
         </div>
     );
 };
 
-export default FirebaseTesting;
+const mapStateToProps = (state) => {
+    return {
+        bIsLoggedIn: state.auth.bIsLoggedIn
+    };
+}
+
+export default connect(mapStateToProps)(FirebaseTesting); 
