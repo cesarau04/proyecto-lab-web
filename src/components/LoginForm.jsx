45f919284294f36c343aa057632afb80041e88ba
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-06-10 01:52:01 -0500

    added login and signup error handling, added notification, not quite working
    Signed-off-by: Cesar Augusto Garcia Perez <cesarau04@gmail.com>

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,8 +5,10 @@ import myFirebaseInstance from '../firebase/myfirebase'
 import { LogInAction, UserIdAction } from "../actions/index";
 
 import { useHistory } from "react-router-dom";
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 import './StayNear.css';
+// import 'react-notifications/lib/Notifications.css'
 
 const LoginForm = (props) => {
     const emailRef = useRef(null);
@@ -14,7 +16,6 @@ const LoginForm = (props) => {
     const history = useHistory();
 
     const handleLogin = () => {
-
         const myFirebase = myFirebaseInstance.getInstance();
 
         let currentEmail = emailRef.current.value
@@ -22,44 +23,60 @@ const LoginForm = (props) => {
 
         if (currentEmail !== "" && currentEmail && currentPassword !== "" && currentPassword) {
             myFirebase.auth().signInWithEmailAndPassword(currentEmail, currentPassword)
-                .catch((error) => { console.error(error); })
                 .then((data) => {
                     props.dispatch(LogInAction(true))
                     props.dispatch(UserIdAction(data.user.uid))
                     localStorage.setItem('userid', data.user.uid)
                     history.push("/home");
                 })
+                .catch((error) => {
+                    localStorage.clear();
+                    switch (error.code) {
+                        case "auth/invalid-email":
+                            NotificationManager.error("Bad credentials", "Email or password are wrong", 4000)
+                        case "auth/wrong-password":
+                            NotificationManager.error("Bad credentials", "Email or password are wrong", 4000)
+                        case "auth/user-not-found":
+                            NotificationManager.error("No user", "No user was found, try sign up", 4000)
+                        default:
+                            NotificationManager.error("Unknown", "A unknown error occurred", 4000)
+                    }
+                    console.log(error);
+                })
         }
     };
 
     return (
-        <div className="bgCr1 right-medium-sign right z-depth-3 waves-effect waves-light">
-            <div className="row center bold">
-                <h5 className="txtWhite mrgTopCorrection">Welcome</h5>
-            </div>
-
-            <div className="row">
-                <div className="input-field col s12">
-                    <input placeholder="Email" id="input_email" type="email" name="aemail" className="validate" ref={emailRef} required />
-                    <label htmlFor="email"></label>
+        <>
+            <div className="bgCr1 right-medium-sign right z-depth-3 waves-effect waves-light">
+                <div className="row center bold">
+                    <h5 className="txtWhite mrgTopCorrection">Welcome</h5>
                 </div>
-            </div>
 
-
-            <div className="row">
-                <div className="input-field col s12">
-                    <input placeholder="password" id="input_password" type="password" name="apassword" className="validate" ref={passwordRef} required />
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input placeholder="Email" id="input_email" type="email" name="aemail" className="validate" ref={emailRef} required />
+                        <label htmlFor="email"></label>
+                    </div>
                 </div>
-            </div>
 
 
-
-            <div className="row center">
-                <div className="col s12">
-                    <button className="bold btn waves-effect waves-light  btn-large blue " type="submit" onClick={handleLogin}><p className="elegant mrgTopCorrection">Take me Home</p></button>
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input placeholder="password" id="input_password" type="password" name="apassword" className="validate" ref={passwordRef} required />
+                    </div>
                 </div>
-            </ div>
-        </div>
+
+
+
+                <div className="row center">
+                    <div className="col s12">
+                        <button className="bold btn waves-effect waves-light  btn-large blue " type="submit" onClick={handleLogin}><p className="elegant mrgTopCorrection">Take me Home</p></button>
+                    </div>
+                </ div>
+            </div>
+            <NotificationContainer />
+        </>
     );
 }
 
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { connect } from 'react-redux';
+import myFirebaseInstance from '../firebase/myfirebase'
+import { LogInAction, UserIdAction } from "../actions/index";
+
+import { useHistory } from "react-router-dom";
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+
 import './StayNear.css';
 
-const SignupForm = () => {
+
+const SignupForm = (props) => {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const history = useHistory();
+
+    const handleSignUp = () => {
+        const myFirebase = myFirebaseInstance.getInstance();
+
+        let currentEmail = emailRef.current.value
+        let currentPassword = passwordRef.current.value
+
+        if (currentEmail !== "" && currentEmail && currentPassword !== "" && currentPassword) {
+            firebase.auth().createUserWithEmailAndPassword(currentEmail, currentPassword)
+                .then((data) => {
+                    NotificationManager.success('Created', 'Account was created', 4000)
+                    props.dispatch(LogInAction(true))
+                    props.dispatch(UserIdAction(data.user.uid))
+                    localStorage.setItem('userid', data.user.uid)
+                    history.push("/home");
+                    const storage = firebase.storage()
+                    storage.ref().child("users").child(data.user.uid).child("profile_pic").putString("64byte profile.png")
+                    // .then((snapshot) => {
+                    //     console.log(snapshot);
+                    //     const db = firebase.database()
+                    //     db.ref().ref('users').child(data.user.uid).child('profile')
+                    // });
+                })
+                .catch((error) => {
+                    NotificationManager.error("ERROR", "Couldn't create account", 4000)
+                })
+        }
+    };
+
     return (
         <div className="bgCr1 right-medium-sign right z-depth-3">
             <div className="row center bold">
@@ -11,7 +51,7 @@ const SignupForm = () => {
 
             <div className="row">
                 <div className="input-field col s12">
-                    <input placeholder="Email" id="input_email" type="email" name="aemail" className="validate" required />
+                    <input placeholder="Email" id="input_email" type="email" name="aemail" className="validate" ref={emailRef} required />
                     <label htmlFor="email"></label>
                 </div>
             </div>
@@ -19,11 +59,11 @@ const SignupForm = () => {
 
             <div className="row">
                 <div className="input-field col s12">
-                    <input placeholder="password" id="input_password" type="password" name="apassword" className="validate" required/>
+                    <input placeholder="password" id="input_password" type="password" name="apassword" className="validate" ref={passwordRef} required />
                 </div>
             </div>
 
-            
+
 
             <div className="row center">
                 <div className="col s12">
@@ -33,4 +73,11 @@ const SignupForm = () => {
         </div>
     );
 }
-export default SignupForm;
+
+const mapStateToProps = (state) => {
+    return {
+        bIsLoggedIn: state.auth.bIsLoggedIn
+    };
+}
+
+export default connect(mapStateToProps)(SignupForm);
